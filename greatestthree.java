e2f8f8159c1378d3c38ec919b5ef1cd45d00d5f8
Deepikavasanthi <deepikamailtome@gmail.com>
2016-07-16 00:08:12 -0700

    Add files via upload

---
+++ greatestthree.java
@@ -0,0 +1,14 @@
+Scanner sc=new Scanner(System.in);
+     int x=sc.nextInt();
+      int y=sc.nextInt();
+      int z=sc.nextInt();
+ if(x>y)
+ {if(x>z){
+             System.out.println("x greater");
+ }if(y>z){
+             System.out.println("y greater");
+ }
+ else{
+             System.out.println("z greater");
+ }
+ }
